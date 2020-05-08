8463463086a883926783cec4932e2a62379e1cae
kazudmb <nakano.kazuki.dmb@gmail.com>
2020-05-08 19:32:00 +0900

    add NavigationView, NavigationLink

--- StampCardAdmin/ContentView.swift
+++ StampCardAdmin/ContentView.swift
@@ -10,7 +10,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            VStack {
+                NavigationLink(destination: QRCodeScanView()) {
+                    Text("QRコードを読み取る")
+                }
+            }
+            .navigationBarTitle(Text("Stamp Card Admin"), displayMode:.inline)
+        }
     }
 }
 

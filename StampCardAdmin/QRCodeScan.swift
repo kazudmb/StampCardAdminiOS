81c35ca515df63b7803a6635d3990e80d696f8a4
kazudmb <nakano.kazuki.dmb@gmail.com>
2020-05-08 19:13:36 +0900

    add File QRCodeScan.swift

---
+++ StampCardAdmin/QRCodeScan.swift
@@ -0,0 +1,21 @@
+//
+//  QRCodeScan.swift
+//  StampCardAdmin
+//
+//  Created by KazukiNakano on 2020/05/08.
+//  Copyright © 2020 kazudmb. All rights reserved.
+//
+
+import SwiftUI
+
+struct QRCodeScan: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct QRCodeScan_Previews: PreviewProvider {
+    static var previews: some View {
+        QRCodeScan()
+    }
+}
